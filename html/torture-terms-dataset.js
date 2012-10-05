fe5891e7f0ace3ef1c4b48076eecee087a47cdd7
Daniel Toman <daniel@Daniel-iMac.local>
2012-10-05 14:47:52 -0700

    proof of concept

---
+++ html/torture-terms-dataset.js
@@ -0,0 +1,37 @@
+// Torture term frequency by outlet
+
+			var Default_terms = [
+				{text: "torture", weight: 5},
+				{text: "enhanced interrogation", weight: 4},
+				{text: "interrogation", weight: 1},
+				{text: "coerced interrogation", weight: 1},
+				{text: "harsh interrogation", weight: 1},
+				{text: "strong interrogation", weight: 1},
+				{text: "tough interrogation", weight: 1},
+				{text: "rough interrogation", weight: 1},
+				{text: "harsh questioning", weight: 1},
+				{text: "abuse", weight: 1},
+				{text: " ", weight: 10}
+			];
+
+			var Fox_terms = [
+				{text: "torture", weight: 107},
+				{text: "enhanced interrogation", weight: 139},
+				{text: "interrogation", weight: 113},
+				{text: "coerced interrogation", weight: 26},
+				{text: "abuse", weight: 10},
+				{text: "harsh interrogation", weight: 8},
+				{text: "strong interrogation", weight: 2},
+				{text: "tough interrogation", weight: 1},
+				{text: "rough interrogation", weight: 1},
+				{text: "harsh questioning", weight: 1}
+			];
+
+			var AP_terms = [
+				{text: "torture", weight: 106},
+				{text: "enhanced interrogation", weight: 5},
+				{text: "interrogation", weight: 13},
+				{text: "harsh interrogation", weight: 6},
+				{text: "abuse", weight: 6},
+				{text: "harsh questioning", weight: 2}
+			];
